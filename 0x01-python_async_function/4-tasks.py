2c612638b4f1901fac6274a0b76bd89a934f2319
Orcha02 <orcha2002@gmail.com>
2022-02-01 21:07:22 -0500

    Fix task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-"""
-Take the code from wait_n and alter it into a new function task_wait_n.
-The code is nearly identical to wait_n except task_wait_random is
-being called.
-"""
+"""Take the code from wait_n and alter it into a new function task_wait_n"""
 import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """return the list of all the delays (float values)"""
-    delay_list: List[float] = []
-    for i in range(n):
-        delay_list.append(await task_wait_random(max_delay))
-    return sorted(delay_list)
+    """calls wait_random and spawns it n times asynchronously"""
+    queue, array = [], []
+
+    for _ in range(n):
+        queue.append(task_wait_random(max_delay))
+
+    for q in asyncio.as_completed(queue):
+        result = await q
+        array.append(result)
+
+    return array
