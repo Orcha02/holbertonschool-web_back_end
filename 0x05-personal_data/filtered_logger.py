a1371a2e6659c8089b4142158198792341bc07ba
Orcha02 <orcha2002@gmail.com>
2022-02-11 13:26:20 -0500

    Add Log formatter

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """ Function called filter_datum that returns the log message obfuscated """
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -21,3 +22,21 @@ def filter_datum(fields: List[str], redaction: str,
         pattern = info + "=.+?(?=abc)*\\" + ";"
         message = re.sub(pattern, info + "=" + redaction + separator, message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Constructor"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records using filter_datum"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
