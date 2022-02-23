3e1b780bd7a7e4ce7be426d623dc916e8d0d8603
Orcha02 <orcha2002@gmail.com>
2022-02-23 14:04:19 -0500

    Trying to fix 1 checker

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,39 +1,38 @@
 #!/usr/bin/env python3
-""" Database module """
+"""DB module
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm import sessionmaker
-from typing import TypeVar
+from sqlalchemy.orm.session import Session
 
 from user import Base, User
 
 
 class DB:
-    """ Database class
-        Creates engine, session, adds user object to DB
-        Methods:
-            add_user - save the user object to the database
-            find_user_by - returns the first row found in the users table
+    """DB class
     """
-    def __init__(self):
-        """ Constructor"""
-        self._engine = create_engine("sqlite:///a.db")
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self):
-        """ Create session """
+    def _session(self) -> Session:
+        """Memoized session object
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
-        """ Add a user instance to the session DB """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ Add a user to the DataBase. """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
-        return user
+        return new_user
--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
+"""
+SQLAlchemy model: User
+"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
 class User(Base):
     """
-    User class
-    Create a SQLAlchemy model named User for a database table named users
+    SQLAlchemy model: User
+    Args:
+        id: the integer primary key
+        email: a non-nullable string
+        hashed_password: a non-nullable string
+        session_id: a nullable string
+        reset_token: a nullable string
     """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
