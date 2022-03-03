f5df664b81f96288440c296240e9f8e850f995fc
Orcha02 <orcha2002@gmail.com>
2022-03-03 17:00:59 -0500

    Add Basic Babel setup

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """ Basic Flask app """
 from flask import Flask, render_template
+from flask_babel import Babel
 app = Flask(__name__)
+babel = Babel(app)
 
+
+class Config(object):
+    """ babel config class """
+    LANGUAGES = ["en", "fr"]
+
+@app.config.from_object(Config)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
 
 @app.route("/", methods=["GET"])
 def index():
