eeafde12c2abd639fa38fdc97b08c469f1234088
Orcha02 <orcha2002@gmail.com>
2022-03-03 17:59:33 -0500

    Get locale from request

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -2,6 +2,7 @@
 """ Basic Flask app """
 from flask import Flask, render_template
 from flask_babel import Babel
+from os import getenv
 app = Flask(__name__)
 babel = Babel(app)
 
@@ -17,8 +18,16 @@ class Config(object):
 @app.route("/", methods=["GET"])
 def index():
     """ Returns index """
-    return render_template("1-index.html")
+    return render_template("2-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """ Determine the best match with our supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
