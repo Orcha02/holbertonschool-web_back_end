71c0cfcb927000c7e0a6bfe0e1acdfe478acb93d
Orcha02 <orcha2002@gmail.com>
2022-03-03 23:21:42 -0500

    Trying to fix checker

--- 0x0A-i18n/4-app.py
+++ 0x0A-i18n/4-app.py
@@ -1,40 +1,37 @@
 #!/usr/bin/env python3
 """ Flask app """
 from flask import Flask, render_template, request
-from os import getenv
 from flask_babel import Babel
+
+
+class Config(object):
+    """ Defines the application configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 babel = Babel(app)
 
-
-class Config(object):
-    """ Config Class."""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
 app.config.from_object(Config)
 
 
-@app.route('/')
-def index():
-    """Render index.html."""
-    return render_template("4-index.html")
-
-
 @babel.localeselector
-def get_locale():
-    """Determine the bestmatch languages."""
-    locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
-        return locale
+def get_locale() -> str:
+    """
+    Determines the best match language according suppported languages
+    """
+    if request.args.get('locale') in app.config['LANGUAGES']:
+        return request.args.get('locale')
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world():
+    """ Welcome HTML page """
+    return render_template('4-index.html')
+
+
 if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host="0.0.0.0", port="5000")
