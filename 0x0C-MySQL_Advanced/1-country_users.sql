aba773ce53416d1c0d85d45d88a37bc7e1039752
Orcha02 <orcha2002@gmail.com>
2022-03-16 14:50:19 -0500

    Add SQL scripts for id,email,name and then country

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- SQL script that creates a table users following these requirements
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
