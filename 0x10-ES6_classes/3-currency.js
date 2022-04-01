42481ddde4c6515a054cc714a9219df2ddd13a06
Orcha02 <orcha2002@gmail.com>
2022-03-31 21:26:14 -0500

    Fix jest

--- 0x10-ES6_classes/3-currency.js
+++ 0x10-ES6_classes/3-currency.js
@@ -1,10 +1,11 @@
+
 export default class Currency {
   constructor(code, name) {
-    if (typeof code === "string") this._code = code;
-    else throw new Error(TypeError("code must be a String"));
+    if (typeof (code) === 'string') this._code = code;
+    else throw new Error(TypeError('code must be a String'));
 
-    if (typeof name === "string") this._name = name;
-    else throw new Error(TypeError("name must be a String"));
+    if (typeof (name) === 'string') this._name = name;
+    else throw new Error(TypeError('name must be a String'));
   }
 
   get code() {
@@ -16,13 +17,13 @@ export default class Currency {
   }
 
   set code(value) {
-    if (typeof value === "string") this._code = value;
-    else throw new Error(TypeError("code must be a String"));
+    if (typeof (value) === 'string') this._code = value;
+    else throw new Error(TypeError('code must be a String'));
   }
 
   set name(value) {
-    if (typeof value === "string") this._name = value;
-    else throw new Error(TypeError("name must be a String"));
+    if (typeof (value) === 'string') this._name = value;
+    else throw new Error(TypeError('name must be a String'));
   }
 
   displayFullCurrency() {
